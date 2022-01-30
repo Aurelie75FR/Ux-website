5b99912adcb0aec97106770579ef9814f7bddc3a
Aurelie75FR <aurelielo.vi@gmail.com>
2022-01-30 19:55:00 +0100

    Ajout du timer pour retour à L'ACCUEIL

--- src/Pages/Remerciement.jsx
+++ src/Pages/Remerciement.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Remerciement() {
+  //https://stackoverflow.com/questions/62861269/attempted-import-error-usehistory-is-not-exported-from-react-router-dom
+  //+
+  //https://stackoverflow.com/questions/63124924/how-to-redirect-to-after-x-seconds-using-react-router-dom
+const history = useNavigate()
+useEffect(()=>{
+  setTimeout(()=>{
+    history('/')
+  }, 5000) //5 secondes
+}, [])
   return <>
       <h1>Merci !</h1>
       <p>Set TimeOut... vers page acceuil</p>
