93abec35e4f1b342cc1726e08756854208f1b076
oliebab <olivier.babiard@gmail.com>
2022-01-31 17:43:50 +0100

    commentaire

--- src/Pages/ReponseVocal.jsx
+++ src/Pages/ReponseVocal.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+// import Countdown from './../Components/Countdown';
 
 function ReponseVocal() {
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const handler = () => {
+  //   setIsPlaying(true)
+  // }
   return <>
-    <button>Start recording</button>
+    <button >Start recording</button>
+    {/* <Countdown />  */}
     <button>
     <Link to='/remerciement'>Stop recording</Link>
     </button>
-    
+      
   </>;
 }
 
