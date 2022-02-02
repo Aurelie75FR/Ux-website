a9567f033dfac391f41213c11ebcc483047d01fa
oliebab <olivier.babiard@gmail.com>
2022-02-02 19:14:31 +0100

    page 1 done

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Video from "./../Assets/Video.mp4";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Button from "./../Components/Button";
 
 const Accueil = () => {
@@ -9,25 +9,23 @@ const Accueil = () => {
 
   // useEffect(() => {});
 
-  const style = {
-      // width: Dimension.get('window').width,
-      // height: Dimension.get('window').height
-      width : "1000px",
-      height : "1600px"
-  }
   //la balise <video>
   //https://developer.mozilla.org/fr/docs/Web/HTML/Element/video
 
   return (
     <div className="accueil-container">
-      
-      <video  width="1620" height="2160" autoPlay muted loop >
+      <video className="video" id="video" autoPlay muted loop>
         <source src={Video} type="video/mp4" />
       </video>
-      
-      
-        {/* <Link to='/question'>to question page</Link> */}
-      
+
+      <p className="touchscreen">
+        Touche l'écran pour nous partager ce qui te rends original·e
+        <br></br>
+        <br></br>
+        Touch the screen to share your originality
+      </p>
+
+      {/* <Link to='/question'>to question page</Link> */}
     </div>
   );
 };
