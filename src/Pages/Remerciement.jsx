5b604f3ed45245aa0d31d4d2088005a560c70430
oliebab <olivier.babiard@gmail.com>
2022-02-03 15:14:13 +0100

    check

--- src/Pages/Remerciement.jsx
+++ src/Pages/Remerciement.jsx
@@ -2,28 +2,25 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Remerciement() {
-  //https://stackoverflow.com/questions/62861269/attempted-import-error-usehistory-is-not-exported-from-react-router-dom
-  //+
-  //https://stackoverflow.com/questions/63124924/how-to-redirect-to-after-x-seconds-using-react-router-dom
-  const navigate = useNavigate();
+  const navigate2 = useNavigate();
   useEffect(() => {
     setTimeout(() => {
-      navigate("/");
-    }, 6000); // 6 secondes 
-  }, [navigate]);
+      navigate2("/");
+    }, 4000); // 4 secondes
+  }, [navigate2]);
 
   return (
     <>
-    <div className="content-thanks">
-      <h1 className="thanks">
-        MERCI D’AVOIR PARTAGÉ <br />
-        TA RÉPONSE AVEC NOUS
-      </h1>
-      <h1 className="thanks">
-        THANK YOU FOR SHARING <br />
-        THAT WITH US
-      </h1>
-    </div>
+      <div className="content-thanks">
+        <h1 className="thanks">
+          MERCI D’AVOIR PARTAGÉ <br />
+          TA RÉPONSE AVEC NOUS
+        </h1>
+        <h1 className="thanks">
+          THANK YOU FOR SHARING <br />
+          THAT WITH US
+        </h1>
+      </div>
       <p className="endQuote">
         Original et pour toujours. <br />
         Always been Always Original
