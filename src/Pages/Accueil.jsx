98888bcb427bed5489879f5d8f1a96c79ff1c78f
oliebab <olivier.babiard@gmail.com>
2022-02-05 22:35:48 +0100

    test meta

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 
 const Accueil = () => {
 
+
+
   return (
     <div className="accueil-container">
-      <button id="goFS">Go fullscreen</button>
+      {/* <button id="goFS">Go fullscreen</button> */}
       <Link className="overlayLink" to="/original">
         <video className="video" id="video" height={1080} width={810}  autoPlay muted loop>
           <source src={Video} type="video/mp4" />
